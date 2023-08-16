055601e6bd16ad81abb255769260fde777cbd18d
danilosheen <c.danilo.f.silva@gmail.com>
2023-08-16 19:13:23 -0300

    refact: updating rodape component

--- src/components/Rodape/rodape.js
+++ src/components/Rodape/rodape.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import './rodape.css'
 
 export default function Rodape(){
     return(
-        <div>
-
-        </div>
+        <footer>
+            <div className="content-rodape">
+                <img className="logo-rodape" src="/assets/logo.png" alt="logo.png" />
+                <div>
+                    <h2>ENDEREÇO</h2>
+                    <p>Av. Bernardino de Campos, 98</p>
+                    <p>São paulo, SP 12345-678</p>
+                </div>
+                <div>
+                    <h2>CONTATO</h2>
+                    <p>info@email.com</p>
+                    <p>Tel: (11)9999-9999</p>
+                </div>
+                <div>
+                    <h2>HORÁRIOS</h2>
+                    <p>Aberto todos os dias</p>
+                    <p>10:00 às 22:00</p>
+                </div>
+            </div>
+            <div className="text-dev-footer">
+                <p>Gelateria desenvolvida por Danilo Ferreira</p>
+            </div>
+        </footer>
     );
 }
