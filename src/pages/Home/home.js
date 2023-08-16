30f8cc627cd4f6d4e10061aa7cc3ebdcce07cdb3
danilosheen <c.danilo.f.silva@gmail.com>
2023-08-16 15:07:36 -0300

    feat: page home

---
+++ src/pages/Home/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Topo from "../../components/Topo/topo";
+
+export default function Home(){
+    return(
+        <div>
+            <Topo/>
+            
+        </div>
+    );
+}
